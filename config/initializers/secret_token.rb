77251c3b7e54c3d92a5f11cd1b41f20e65cae725
dtsong <dtsong@andrew.cmu.edu>
2015-10-30 12:09:40 -0400

    Initial Commit for Deligo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deligo::Application.config.secret_key_base = '7f0b5df9d4644f5e3cecc80199ef04ddd678f19a51ed923f4ad0c02325627778016af199147d1a7420e7d437b430a3c277758ca537276efae076ea589e82273f'
