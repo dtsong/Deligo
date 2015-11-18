c01cb33d6e2744813bae48e0a8c2bbce66db5b41
dtsong <dtsong@andrew.cmu.edu>
2015-11-17 23:17:04 -0500

    added additional validations, scopes, and tests pending

--- app/models/answer_option.rb
+++ app/models/answer_option.rb
@@ -1,3 +1,9 @@
 class AnswerOption < ActiveRecord::Base
+  # Relationships
   belongs_to :questions
+
+  # Validations
+  validates_presence_of :option, :question_id
+  validates_numericality_of :question_id, only_integer: true 
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,14 @@
 class Question < ActiveRecord::Base
+  # Relationships
   belongs_to :user
   has_many :answer_options
   has_many :pictures
   has_many :answers
+
+  # Validations 
+  validates_presence_of :question_text, :creator_id
+
+  # Scopes
+  scope :for_creator, -> (creator_id) { where("creator_id = ?", creator_id) }
+  
 end
