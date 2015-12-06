bfb16747fc921528952dda6324da5f179ded7cb8
Allie Wilson <aswilson@andrew.cmu.edu>
2015-12-06 16:12:08 -0500

    user can't be added to the same group more than once

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,4 +6,17 @@ class Member < ActiveRecord::Base
   validates_numericality_of :user_id, only_integer: true 
   validates_numericality_of :group_id, only_integer: true 
   
+  validate :member_is_not_a_duplicate, on: :create
+  
+  
+  def already_exists?
+    Member.where(user_id: self.user_id, group_id: self.group_id).size == 1
+  end
+  
+  def member_is_not_a_duplicate
+    return true if self.user_id.nil? || self.group_id.nil?
+    if self.already_exists?
+      errors.add(:group_id, "already contains this user")
+    end
+  end
 end
